3817367f3c72592b189f8a4040bb06b7fab81b69
RolDev-22 <rolandomuagui1629@gmail.com>
2023-11-30 09:05:36 -0600

    actualizacion buscar

--- Components/BuscarGeneral.jsx
+++ Components/BuscarGeneral.jsx
@@ -4,33 +4,28 @@ import styles from '@/styles/BuscarGeneral.module.css';
 const BuscarGeneral = () => {
   return (
     <div className={styles.bodyBuscar}>
-      <table style={tableBody }from>
-        <thead>
+      <table className={styles.tableStyle} from>
+        <thead className={styles.theadStyle}>
           <tr>
-            <th>&nbsp;</th>
-            <th>&nbsp;</th>
-            <th>&nbsp;</th>
-            <th>&nbsp;</th>
-            <th>&nbsp;</th>
-            <th>&nbsp;</th>
+            <th className={styles.thStyle}>Resultados de busqueda</th>
           </tr>
         </thead>
-        <tbody>
-          <tr>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
+        <tbody className={styles.tbodyStyle}>
+          <tr className={styles.trStyle}>
+            <th className={styles.thStyle}>Id</th>
+            <th className={styles.thStyle}> Nombre</th>
+            <th className={styles.thStyle}>Id_Proveedor</th>
+            <th className={styles.thStyle}>Cantidad</th>
+            <th className={styles.thStyle}>Unidad</th>
+            <th className={styles.thStyle}>Categoría</th>
           </tr>
-          <tr>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
+          <tr className={styles.trStyle}>
+            <td className={styles.tdStyle}>Id</td>
+            <td className={styles.tdStyle}>Nombre</td>
+            <td className={styles.tdStyle}>Id_Proveedor</td>
+            <td className={styles.tdStyle}>Cantidad</td>
+            <td className={styles.tdStyle}>Unidad</td>
+            <td className={styles.tdStyle}>Categoría</td>
           </tr>
         </tbody>
       </table>
